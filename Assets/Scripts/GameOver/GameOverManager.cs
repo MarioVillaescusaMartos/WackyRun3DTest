124f41761caaa8120ae8dccde354b459ff6ab352
MarioVillaescusaMartos <villaescusam.mario@gmail.com>
2022-03-09 13:49:00 +0100

    Version015
    ADD Temple sun

--- Assets/Scripts/GameOver/GameOverManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class GameOverManager
-{
-    public static bool notShowGameOver;
-}
--- Assets/Scripts/Menu/PauseManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class PauseManager
-{
-    public static bool pauseMode;
-}
